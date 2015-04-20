83c4419a55a3c0e417cae2184b9b135cdf4330ce
Jean-Denis Vauguet <jd@vauguet.fr>
2015-04-20 02:36:46 +0200

    Fixing UserSerializer spec

--- spec/support/example_groups/serializer_example_group.rb
+++ spec/support/example_groups/serializer_example_group.rb
@@ -2,7 +2,7 @@ module SerializerExampleGroup
   extend ActiveSupport::Concern
 
   RSpec.configure do |config|
-    config.include self, 
+    config.include self,
     :type => :serializer,
     :example_group => { :file_apath => %r(spec/serializers) }
   end
