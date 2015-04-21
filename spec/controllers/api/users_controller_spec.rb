a5728d23b12a8d26d3d0693fec991959fb91d04d
Ryan Haase <ryandhaase@gmail.com>
2015-04-21 16:34:55 -0400

    User authentication in progress

---
+++ spec/controllers/api/users_controller_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+
+# RSpec.describe Api::UsersController, type: :controller do
+
+#   # setup { host! 'api.example.com' }
+
+#   it 'returns list of all users' do
+#     get '/users'
+#     assert_equal 200, response.status
+#     refute_empty response.body
+#   end
+# end
